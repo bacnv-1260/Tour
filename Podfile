cf807ddb020886154dbc5aa0967e9f9c2e7b97f6
huediemdue <huediemdue@gmail.com>
2019-06-07 09:50:48 +0700

    Create Splash and Login Screen and logic

--- Podfile
+++ Podfile
@@ -4,8 +4,13 @@
 target 'Tour' do
   # Comment the next line if you don't want to use dynamic frameworks
   use_frameworks!
+  pod 'Firebase/Core'
 	pod 'Firebase/Analytics'
 	pod 'Firebase/Database'
+  pod 'Firebase/Auth'
+  
+  pod 'FBSDKLoginKit'
+  
   # Pods for Tour
 
 end
--- Tour/ViewController.swift
+++ Tour/ViewController.swift
@@ -7,14 +7,71 @@
 //
 
 import UIKit
+import Firebase
+import FBSDKLoginKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var btnLoginFb: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
+    override func viewDidAppear(_ animated: Bool) {
+        if FBSDKAccessToken.current() != nil {
+             self.showAlertLogin()
+            btnLoginFb.isEnabled = false
+        }
+    }
+    
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+    
+    @IBAction func onLoginFb(_ sender: Any) {
+        let LoginManager = FBSDKLoginManager()
+        LoginManager.logIn(withReadPermissions: ["public_profile", "email"], from: self) { (result, error) in
+            if let error = error {
+                self.showAlert(message: "Failed to login: \(error.localizedDescription)")
+                return
+            }
+            guard let accessToken = FBSDKAccessToken.current() else {
+                self.showAlert(message: "Failed to get access token")
+                return
+            }
+            
+            let credential = FacebookAuthProvider.credential(withAccessToken: accessToken.tokenString)
+            Auth.auth().signInAndRetrieveData(with: credential) { (user, error) in
+                if let error = error {
+                    self.showAlert(message: "Login error: \(error.localizedDescription)")
+                    return
+                }
+            }
+        }
+    }
+    
+    func showAlert(message: String) {
+        let alert = UIAlertController(title: "Thông báo",
+                                      message: message ,
+                                      preferredStyle: .alert);
+        alert.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
+        self.present(alert, animated: true)
+    }
+    
+    func showAlertLogin(){
+        let string:String = "Để đảm bảo an toàn cho chuyến đi của bạn và người khác, việc cung cấp thông tin là cần thiết. Thông tin này sẽ được chúng tôi giữa kín và không cung cấp cho một bên thứ 3"
+        let alert = UIAlertController(title: nil,
+                                      message: string,
+                                      preferredStyle: .alert);
+        alert.view.backgroundColor = UIColor.white
+        alert.view.layer.cornerRadius = 10
+        alert.addAction(UIAlertAction(title: "Thêm ngay", style: .cancel, handler: nil))
+        alert.addAction(UIAlertAction(title: "Thêm sau", style: .default, handler: nil))
+        self.present(alert, animated: true)
+    }
+    
+    
+    
 }
 
